5eaf129cfad365c602ff7be37497c3d5cc28d731
Gabriel Bertolini <gabrielbertolini2002@hotmail.com>
2026-06-14 21:44:35 -0300

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 # Configuração institucional da página
 st.set_page_config(
-    page_title="Píer Partners - Sistema de Análise de Risco",
+    page_title="Píer Partners : Sistema de Análise de Risco - Gabriel Bertolini",
     layout="wide"
 )
 
